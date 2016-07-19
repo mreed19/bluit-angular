c26cbe03e35119c7e747324d8673bd44462c0691
mreed19 <michael.reed315@gmail.com>
2016-07-18 23:53:02 -0400

    Initial commit

---
+++ app/app.module.js
@@ -0,0 +1,12 @@
+(function() {
+  angular.module('bluit', [
+    'ui.router',
+    'bluit.posts'
+  ])
+  .config(configFunction);
+
+  configFunction['$inject'] = ['$stateProvider', '$urlRouterProvider'];
+  function configFunction($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/posts');
+  }
+})();
---
+++ app/posts/config.route.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('bluit.posts')
+    .config(configFunction);
+
+  configFunction.$inject = ['$stateProvider'];
+  function configFunction($stateProvider) {
+    $stateProvider
+      .state('posts', {
+        url: '/posts',
+        template: '<h2>posts</h2>'
+      });
+  }
+})();
