fc93a766e30e9f043f84de28d1f5f4651b66fe89
mreed19 <michael.reed315@gmail.com>
2016-07-22 00:47:53 -0400

    Add fields in post form and add styles

--- app/posts/posts-form.directive.js
+++ app/posts/posts-form.directive.js
@@ -6,7 +6,40 @@
         return {
           template: `
 
-          <h1>Posts Form</h1>
+          <form ng-submit="vm.save()">
+            <p>
+              <label for="post_title">Title</label>
+              <input
+                ng-model="vm.post.title"
+                type="text"
+                name="title"
+                id="post_title"
+                placeholder="Title your post"
+                focus-on
+                required>
+            </p>
+            <p>
+              <label for="post_link_url">Link URL</label>
+              <input
+                ng-model="vm.post.link_url"
+                type="url"
+                id="post_link_url"
+                placeholder="URL link">
+            </p>
+            <p>
+              <label for="post_body">Body</label>
+              <textarea
+                ng-model="vm.post.body"
+                id="post_body"
+                placeholder="Post content..."></textarea>
+            </p>
+            <div class="form-actions">
+              <input
+                type="submit"
+                value="Create Note"
+                class="btn btn-default">
+            </div>
+          </form>
 
           `
         };
